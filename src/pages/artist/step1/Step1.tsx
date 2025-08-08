3ad6190fc60dc316342e3664bd3382bab065731f
Helena Glowacki <113125436+helenaglow@users.noreply.github.com>
2025-08-08 18:22:29 -0400

    remove comments

--- src/pages/artist/step1/Step1.tsx
+++ src/pages/artist/step1/Step1.tsx
@@ -25,41 +25,6 @@ const ArtistStep1 = () => {
             </div>
 
         </MultiPageTemplate>
-    //    <PageTemplate background='bg3' title="Step 1: Brainstorm" description='This is your time to familiarize yourself with the text and brainstorm for your poem.' duration={30} afterDuration={onComplete}>
-    //     <div className="w-full h-full flex flex-col md:flex-row md:space-x-12">
-    //         <div className="h-full w-full md:w-1/2 flex">
-    //             <p className="text-main text-sm md:text-base">{passage}</p>
-    //         </div>
-    //             <div className="w-full md:w-1/2 h-full">
-    //             <Tabs.Root lazyMount unmountOnExit defaultValue="tab-1" className="w-full h-full">
-    //                 <Tabs.List className="space-x-4">
-    //                     <Tabs.Trigger value="tab-1">Notes</Tabs.Trigger>
-    //                                 {(userType === 'SPARK' || userType === 'TOTAL_ACCESS'
-    //         ) && (
-    //                     <Tabs.Trigger value="tab-2">LLM</Tabs.Trigger>
-    //         )}
-    //                 </Tabs.List>
-    //                 <Tabs.Content value="tab-2" className="w-full h-4/5">
-    //                    <ChatTab />
-    //                 </Tabs.Content>
-    //                 <Tabs.Content value="tab-1" className="w-full h-4/5">
-    //                     <Textarea
-    //                         value={notes}
-    //                         onChange={(e) => setNotes(e.target.value)}
-    //                         placeholder="Take some notes..."
-    //                         className="text-main h-full flex-1 px-3 py-2 border rounded-md focus:outline-none focus:border-2 focus:border-grey"
-    //                     />
-    //                 </Tabs.Content>
-    //                 </Tabs.Root>
-  
-
-    //         </div>
-
-            
-            
-    //     </div>
-
-    //    </PageTemplate>
     );
 };
 
--- src/pages/artist/step2/Step2.tsx
+++ src/pages/artist/step2/Step2.tsx
@@ -24,46 +24,6 @@ const ArtistStep2 = () => {
             </div>
 
         </MultiPageTemplate>
-        // <PageTemplate background='bg3' title='Step 2: Blackout' description='Write a blackout poem by selecting words in the passage. Let your imagination run wild!' nextButton={{text:"Submit", action: onComplete }}>
-        //     <div className="w-full h-full flex flex-col md:flex-row md:space-x-12">
-
-        //     <div className="w-full md:w-1/2 h-full">
-        //          <BlackoutPoetry/>
-        //     </div>
-
-        //     <div className="w-full md:w-1/2 h-full">
-        //         <Tabs.Root lazyMount unmountOnExit defaultValue="tab-1" className="w-full h-full">
-        //             <Tabs.List className="space-x-4">
-        //                 <Tabs.Trigger value="tab-1">Notes</Tabs.Trigger>
-        //                             {(userType === 'WRITING' || userType === 'TOTAL_ACCESS'
-        //     ) && (
-        //                 <Tabs.Trigger value="tab-2">LLM</Tabs.Trigger>
-        //     )}
-        //             </Tabs.List>
-        //             <Tabs.Content value="tab-2" className="w-full h-4/5">
-        //                <ChatTab />
-        //             </Tabs.Content>
-        //             <Tabs.Content value="tab-1" className="w-full h-4/5">
-        //                 <Textarea
-        //                     value={notes}
-        //                     onChange={(e) => setNotes(e.target.value)}
-        //                     placeholder="Take some notes..."
-        //                     className="text-main h-full flex-1 px-3 py-2 border rounded-md focus:outline-none focus:border-2 focus:border-grey"
-        //                 />
-        //             </Tabs.Content>
-        //             </Tabs.Root>
-  
-
-        //     </div>
-        //     </div>
-           
-
-        // </PageTemplate>
-
-
-       
-
-       
     );
 };
 
