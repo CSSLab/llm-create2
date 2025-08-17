6edfb727e7697cad2c9a6a00bfb097bd2c8ed167
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2025-08-17 15:22:56 -0400

    ahhhh

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,6 +14,7 @@ export const aiResponse = onCall(async (data) => {
     });
 
     return { text: response.output_text };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
     console.error(err);
     throw new HttpsError("internal", err.message);
